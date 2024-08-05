cce454e16c8d33ffacd4950afe55ffa5838cfe8d
Mahak <mahakchauhan2004@gmail.com>
2024-08-05 23:12:26 +0530

    changes

--- api/db/db.js
+++ api/db/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 require("../model/provider.model");
 
-// const mongodbUri = "mongodb://127.0.0.1:27017/providers";
+const mongodbUri = "mongodb://127.0.0.1:27017/provider_db";
 
-const mongodbUri =
-  "mongodb+srv://mahakchauhan2004:qM4bashkAcRZyIyO@cluster0.xzbvkvg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const mongodbUri =
+//   "mongodb+srv://mahakchauhan2004:qM4bashkAcRZyIyO@cluster0.xzbvkvg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose
   .connect(mongodbUri)
   .then(() => {
