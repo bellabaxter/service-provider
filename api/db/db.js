11fd61f154b09a9ba7debd6c2ce959586a2de693
Mahak <mahakchauhan2004@gmail.com>
2024-08-06 10:45:31 +0530

    vercel

--- api/db/db.js
+++ api/db/db.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 
 //const mongodbUri = "mongodb://127.0.0.1:27017/provider_db";
 
-const mongodbUri =process.env.URI;
+const mongodbUri ="mongodb+srv://mahakchauhan2004:uZHFdC4CSp3Kl9wD@mymongodb.8l9mhql.mongodb.net/?retryWrites=true&w=majority&appName=mymongodb";
 mongoose
   .connect(mongodbUri)
   .then(() => {
