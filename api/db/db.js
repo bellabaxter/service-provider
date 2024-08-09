6030decdf0e87dec2c4906d7185f5afe29bf06bc
Mahak <mahakchauhan2004@gmail.com>
2024-08-09 23:43:11 +0530

    deploy

--- api/db/db.js
+++ api/db/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 require("../model/provider.model");
-import 'dotenv/config';
+
 
 //const mongodbUri = "mongodb://127.0.0.1:27017/provider_db";
 
-const mongodbUri ="mongodb+srv://mahakchauhan2004:uZHFdC4CSp3Kl9wD@mymongodb.8l9mhql.mongodb.net/?retryWrites=true&w=majority&appName=mymongodb";
+const mongodbUri = process.env.URI;
 mongoose
   .connect(mongodbUri)
   .then(() => {
